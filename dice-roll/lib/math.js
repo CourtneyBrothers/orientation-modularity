20d1747fc2b979a274303e68eed6cb4d850b6bca
Courtney Brothers <courtneywbrothers@gmail.com>
2018-03-06 04:58:16 -0600

    fix parethesis on random int function

--- dice-roll/lib/math.js
+++ dice-roll/lib/math.js
@@ -4,6 +4,6 @@ module.exports = function(lowBound, upperBound){
    console.log(lowBound, upperBound, "low", "up")
    console.log(Math.floor(Math.random()* (upperBound - lowBound + 1)), "math");
 
-    return Math.floor(Math.random()* (upperBound - lowBound + 1) + lowBound);
+    return Math.floor(Math.random()* (upperBound - lowBound + 1)) + lowBound;
 
 }
