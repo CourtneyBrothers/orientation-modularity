c9ed7ce60ccc909a3fbc866e88052078ebc6c9a6
Courtney Brothers <courtneywbrothers@gmail.com>
2018-03-07 14:49:49 -0600

    parseArgs test

--- dice-roll/test/dice-test.js
+++ dice-roll/test/dice-test.js
@@ -1,5 +1,5 @@
 const {roll,toDiceNotation}= require("../lib/dice");
-const {assert:{isFunction,isNumber,isString}} = require("chai");
+const {assert:{isFunction,isNumber,isString,property,oneOf}} = require("chai");
 
 describe("dice module", () => {
     describe("roll",()=>{
@@ -9,11 +9,18 @@ describe("dice module", () => {
         it('result of dice notation string enter should be a number', ()=>{
             isNumber(roll('1d20'));
         });
+        it ("should accept a string and return a computed nomber",()=>{
+            for (let i=0; i<10; i++){
+                let diceNotation = `${i}d6`;
+                oneOf(roll(diceNotation),Array.from(Array(61).keys()));
+            };
+        });
     });
     describe("toDiceNotation",()=>{
         it("should return a string",()=>{
             isString(toDiceNotation(2,4));
         });
+        
     });
 });
 
--- dice-roll/test/parse-args.js
+++ dice-roll/test/parse-args.js
@@ -1,10 +1,10 @@
-const {count,sides}= require("../lib/parse-args");
-const {assert} = require("chai");
+const parseArgs = require("../lib/parse-args");
+const {assert:{isObject}} = require("chai");
 
-describe.skip("roll",()=>{
-    describe("count",()=>{
-        it("should return sides = arg[3] if arg[3] exists",()=>{
-            assert.equal(arg[3] = array[3])
+describe("Parse Args",()=>{
+    describe("Parse Args",()=>{
+        it(" should return an object ",()=>{
+            isObject(parseArgs());
         });
     });
 });
